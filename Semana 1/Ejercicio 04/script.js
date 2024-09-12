73a431cea1fd4e844604ba12bbf33c7ccf4f98bb
Carlos Gauto <96149440+23carlosg@users.noreply.github.com>
2024-09-12 10:40:21 -0300

    Correccion de boton estilo normal

--- Semana 1/Ejercicio 04/script.js
+++ Semana 1/Ejercicio 04/script.js
@@ -4,13 +4,12 @@ let h2 = document.querySelector("h2");
 let buttons = document.querySelectorAll("button");
 
 function estiloNormal() {
-    form.classList.toggle("bg-form");
-    labels.forEach(label => label.classList.toggle("txt-white"));
-    h2.classList.toggle("txt-white");
+    form.classList.remove("bg-form");
+    labels.forEach(label => label.classList.remove("txt-white"));
+    h2.classList.remove("txt-white");
     buttons.forEach(btn => {
-        btn.classList.toggle("bg-yellow");
-        btn.classList.toggle("txt-black");
-        btn.classList.remove("btn-primary");
+        btn.classList.remove("bg-yellow");
+        btn.classList.remove("txt-black");
     });
 }
 
